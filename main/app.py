ab137135d2ac44554147bf9bd1fb02c6ec76fac9
Nicholas Nielsen <nicknielsen5@gmail.com>
2023-04-14 00:35:05 +0000

    ay ay ay

--- main/app.py
+++ main/app.py
@@ -6,6 +6,15 @@ from Crypto.Random import get_random_bytes
 app = Flask(__name__)
 
 #User Vars
+username = ""
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route('/')
 def index():
@@ -18,7 +27,6 @@ def portfolio():
 @app.route('/about')
 def about():
     return render_template('about.html')
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
